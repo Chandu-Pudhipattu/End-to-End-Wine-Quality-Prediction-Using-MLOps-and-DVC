7752f73f76cde8c6af5961b03c9ba4ca0761d4ee
Chandu <chanducn3109@gmail.com>
2025-04-01 20:05:11 -0500

    app updated

--- app.py
+++ app.py
@@ -59,5 +59,4 @@ def index():
         return render_template("index.html")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Use $PORT if available, else 5000
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
